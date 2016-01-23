8a7bb33c6db7d0443382cb07f73f5d31d286a1be
chen29011 <907172474@qq.com>
2016-01-23 15:31:03 +0800

    新增工具类项目

---
+++ user-common/src/main/java/com/cqlj/user/common/App.java
@@ -0,0 +1,13 @@
+package com.cqlj.user.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
