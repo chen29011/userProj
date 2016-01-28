3213fe56a7d09c43d5f10427aa969609e3200d71
chen29011 <907172474@qq.com>
2016-01-28 17:37:56 +0800

    密码工具类

---
+++ user-common/src/main/java/com/cqlj/user/common/decryption/base64/Base64Util.java
@@ -0,0 +1,17 @@
+package com.cqlj.user.common.decryption.base64;
+
+import java.io.IOException;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+public class Base64Util {
+	public static String encryptBase64(byte[] data) {
+		return new BASE64Encoder().encode(data);
+	}
+
+	public static String decryptBase64(String data) throws IOException {
+		byte[] resultBytes = new BASE64Decoder().decodeBuffer(data);
+		return new String(resultBytes);
+	}
+}
