998b5ba873d883db442f2bf45bf1fd3fff4fa067
chen29011 <907172474@qq.com>
2016-01-28 09:15:35 +0800

    提交更新

--- user-common/src/main/java/com/cqlj/user/common/barCode/qrCode/QrCodeUtilsTest.java
+++ user-common/src/main/java/com/cqlj/user/common/barCode/qrCode/QrCodeUtilsTest.java
@@ -16,9 +16,9 @@ public class QrCodeUtilsTest {
 	public void qrEncodeTest() throws WriterException, IOException {
 		String url = "http://www.dijiaapp.com?key=";
 		JSONObject json = new JSONObject();
-		json.put("headId", "11110001");
-		json.put("branchId", "11110000");
-		json.put("seatNo", "二楼清华!园");
+		json.put("headId", "1");
+		json.put("branchId", "2");
+		json.put("seatNo", "一楼22号");
 		Path path = FileSystems.getDefault().getPath("D://", "zxing.png");
 		QrCodeUtils.qrEncode(path, 200, 200, "png", url + json.toJSONString());
 	}
