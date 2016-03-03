22e3cd607e519ab96e26921d0da7f400c05ba57b
chen29011 <907172474@qq.com>
2016-03-03 17:07:04 +0800

    提交更新

--- user-cy/src/main/java/com/cqlj/user/cy/TestController.java
+++ user-cy/src/main/java/com/cqlj/user/cy/TestController.java
@@ -25,17 +25,18 @@ public class TestController {
 		String img_url = "http://i13.tietuku.com/e5ca267b810d722b.png";
 		String profile_url = "http://www.chenqiong.net/my/index.html";
 		String sign = "";
-		/*User user = new User();
+		User user = new User();
 		user.setUser_id(Integer.parseInt(user_id));
 		user.setNickname(nickname);
 		user.setImg_url(img_url);
 		user.setProfile_url(profile_url);
 		user.setSign(sign);
-		HttpSession session = res.getSession();
-		session.setAttribute("user", user);
-		User user2 = (User)session.getAttribute("user");*/
-		Cookie cookie_user_id = new Cookie("user_id", user_id);
-		resp.addCookie(cookie_user_id);
+		resp.addCookie(new Cookie("user_id", user_id));
+		resp.addCookie(new Cookie("nickname","chen29011"));
+		resp.addCookie(new Cookie("img_url","http://i13.tietuku.com/e5ca267b810d722b.png"));
+		resp.addCookie(new Cookie("profile_url", "http://www.chenqiong.net/my/index.html"));
+		resp.addCookie(new Cookie("sign",""));
+		
 		//System.out.println(user2.getUser_id());
 		res.getRequestDispatcher("../topic.jsp").forward(res, resp);
 	}
@@ -43,10 +44,10 @@ public class TestController {
 	// 该接口只有当畅言已登录，getUserInfo返回未登录时，才会被调用用来登录自身网站
 	@RequestMapping(value = "login")
 	public void loginByCy(@RequestParam(value = "callback") String callback,HttpServletRequest res,HttpServletResponse resp) throws IOException {
-		// 自己网站的登录逻辑，记录登录信息到Session中
-		HttpSession session = res.getSession();
-		User user = (User)session.getAttribute("user");
-		int user_id = user.getUser_id();
+		//自己网站的登录逻辑，记录登录信息到cookie
+		int user_id = 1234567;
+		Cookie cookie = new Cookie("user_id", user_id+"");
+		resp.addCookie(cookie);
 		resp.getWriter().write(callback + "({\"user_id\":" + user_id + ",reload_page:0})");
 	}
 	@RequestMapping("loginout")
@@ -68,16 +69,12 @@ public class TestController {
             userinfo.setIs_login(1);//用户已登录
             User user = new User();
             user.setUser_id((Integer)getCookieValue("user_id",cookies)); //该值具体根据自己用户系统设定
-            String nickname = "chen29011";
-    		String img_url = "http://i13.tietuku.com/e5ca267b810d722b.png";
-    		String profile_url = "http://www.chenqiong.net/my/index.html";
-    		String sign = "";
-    		user.setNickname(nickname);
-    		user.setImg_url(img_url);
-    		user.setProfile_url(profile_url);
-    		user.setSign(sign);
+            user.setImg_url((String)getCookieValue("img_url",cookies));
+            user.setNickname((String)getCookieValue("nickname",cookies));
+            user.setProfile_url((String)getCookieValue("profile_url",cookies));
+            user.setSign((String)getCookieValue("sign",cookies));
             userinfo.setUser(user);
-        }
+       }
 		resp.setContentType("application/x-javascript");// 指定传输格式为js
 		resp.getWriter().write(callback + "(" + JSONArray.toJSONString(userinfo) + ")");// 拼接成jsonp格式
 	}
--- user-cy/src/main/webapp/login.jsp
+++ user-cy/src/main/webapp/login.jsp
@@ -8,7 +8,6 @@
 </head>
 <body>
 	<form action="cy/myLogin" method="get">
-
 		用户名： <input type="text" name="user_id" value="123456" /><br> 密码：
 		<input type="password" name="password" value="admin" /><br> <input
 			type="submit" value="登录">
